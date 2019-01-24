beb381b823c0e011dd3feb972ec72a58375d09f0
Dreamwalker <aristojeff@gmail.com>
2019-01-24 21:51:21 +0900

    Update colors.dart

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -4,3 +4,9 @@ import 'dart:ui';
 
 
 Color firstColor = Color(0xFF7A36DC);
+Color secondColor = Color(0xFF7A36DC).withOpacity(0.5);
+Color thirdColor = Color(0xFF7A36DC).withOpacity(0.2);
+
+final appTheme = ThemeData(
+  primarySwatch: Colors.blue,
+);
