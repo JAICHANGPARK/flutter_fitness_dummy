849bf3684e0d12116853dc9efbec18d57b1cf094
Dreamwalker <aristojeff@gmail.com>
2019-01-24 22:54:02 +0900

    Update radial_progress.dart

--- lib/appwidget/radial_progress.dart
+++ lib/appwidget/radial_progress.dart
@@ -8,7 +8,22 @@ class RadialProgress extends StatefulWidget {
   _RadialProgressState createState() => _RadialProgressState();
 }
 
-class _RadialProgressState extends State<RadialProgress> {
+class _RadialProgressState extends State<RadialProgress>
+    with SingleTickerProviderStateMixin {
+  AnimationController _radialProgressAnimationController;
+  Animation<double> _progressAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _radialProgressAnimationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 3));
+
+    _progressAnimation = Tween(begin: 0.0, end: 360.0).animate(_radialProgressAnimationController)
+    ..addListener(listener);
+  }
+
   @override
   Widget build(BuildContext context) {
     return CustomPaint(
@@ -16,12 +31,16 @@ class _RadialProgressState extends State<RadialProgress> {
         height: 200.0,
         width: 200.0,
       ),
-      painter: RadialPainter(),
+      painter: RadialPainter(250),
     );
   }
 }
 
 class RadialPainter extends CustomPainter {
+  double progressInDegrees;
+
+  RadialPainter(this.progressInDegrees);
+
   @override
   void paint(Canvas canvas, Size size) {
     Paint paint = Paint()
@@ -34,7 +53,6 @@ class RadialPainter extends CustomPainter {
     canvas.drawCircle(center, size.width / 2, paint);
 
     Paint progressPaint = Paint()
-    
       ..shader = LinearGradient(
               colors: [Colors.red, Colors.purple, Colors.purpleAccent])
           .createShader(Rect.fromCircle(center: center, radius: size.width / 2))
@@ -42,8 +60,12 @@ class RadialPainter extends CustomPainter {
       ..style = PaintingStyle.stroke
       ..strokeWidth = 8.0;
 
-    canvas.drawArc(Rect.fromCircle(center: center, radius: size.width / 2),
-        math.radians(-90), math.radians(250), false, progressPaint);
+    canvas.drawArc(
+        Rect.fromCircle(center: center, radius: size.width / 2),
+        math.radians(-90),
+        math.radians(progressInDegrees),
+        false,
+        progressPaint);
   }
 
   @override
