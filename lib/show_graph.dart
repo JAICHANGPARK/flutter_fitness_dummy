940e2be13299857a03767a7dd3f63f11baaa69cd
Dreamwalker <aristojeff@gmail.com>
2019-01-26 13:07:50 +0900

    Update show_graph.dart

--- lib/show_graph.dart
+++ lib/show_graph.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 
-
 class ShowGraph extends StatefulWidget {
   @override
   _ShowGraphState createState() => _ShowGraphState();
 }
 
-class _ShowGraphState extends State<ShowGraph> {
+class _ShowGraphState extends State<ShowGraph>
+    with SingleTickerProviderStateMixin<ShowGraph> {
+  AnimationController _graphAnimationController;
+
+  @override
+  void initState() {
+    _graphAnimationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _graphAnimationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
+          alignment: Alignment.center,
           child: Container(
             color: Colors.grey,
             height: 100.0,
